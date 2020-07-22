93e7f0534c5178f17ceb336cd0ef678433d478df
TEDDY LIN (ITA-ISDC-ISD-OOCLL/ZHA) <LINTE@OOCL.COM>
2020-07-22 16:24:42 +0800

    [ITA-CLASS2-GROUP2][TEDDY.LIN][FEATURE]:accomplish practice07

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,21 @@
 package practice07;
 
 public class Klass {
+    public String number;
+
+    public Klass(int number) {
+        this.number = String.valueOf(number);
+    }
+
+    public String getDisplayName(){
+        return "Class 2";
+    }
+
+    public int getNumber() {
+        return Integer.parseInt(this.number);
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
 }
--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,35 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person {
+
+    private Klass klass;
+
+    public Teacher(String name, int age,Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name,int age){
+        super(name,age);
+    }
+
+    @Override
+    public String introduce() {
+        return (klass==null)?super.introduce()+" I am a Teacher. I teach No Class."
+                :super.introduce()+" I am a Teacher. I teach "+this.klass.getDisplayName()+".";
+    }
+
+    public String introduceWith(Student student){
+        return (student.getKlass().getNumber()==this.klass.getNumber())
+                ?super.introduce()+" I am a Teacher. I teach "+student.getName()+"."
+                    :super.introduce()+" I am a Teacher. I don't teach "+student.getName()+".";
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
 }
