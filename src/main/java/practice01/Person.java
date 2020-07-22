cb24b15a4272bdf6af5496aaba618f9d0fdb006c
TEDDY LIN (ITA-ISDC-ISD-OOCLL/ZHA) <LINTE@OOCL.COM>
2020-07-22 15:24:51 +0800

    [ITA-CLASS2-GROUP2][TEDDY.LIN][FEATURE]:accomplish practice01

--- src/main/java/practice01/Person.java
+++ src/main/java/practice01/Person.java
@@ -1,4 +1,33 @@
 package practice01;
 
 public class Person {
+
+    private String name;
+
+    private int age;
+
+    public Person(String name,int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public String introduce(){
+        return "My name is "+this.name+". I am "+this.age+" years old.";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
